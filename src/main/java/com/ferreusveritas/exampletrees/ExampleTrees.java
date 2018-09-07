edd67decde6a5292f3fbfe7c61e26cc93e1c4784
ferreusveritas <ferreusveritas@gmail.com>
2018-09-07 09:42:58 -0400

    Convert to registerable IBiomeDataBasePopulator system

--- src/main/java/com/ferreusveritas/exampletrees/ExampleTrees.java
+++ src/main/java/com/ferreusveritas/exampletrees/ExampleTrees.java
@@ -5,10 +5,10 @@ import com.ferreusveritas.dynamictrees.api.WorldGenRegistry;
 import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.EnumChance;
 import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.ISpeciesSelector;
 import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.StaticSpeciesSelector;
+import com.ferreusveritas.dynamictrees.api.worldgen.IBiomeDataBasePopulator;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase;
 import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase.Operation;
-import com.ferreusveritas.dynamictrees.worldgen.TreeGenerator;
 import com.ferreusveritas.exampletrees.proxy.CommonProxy;
 
 import net.minecraft.util.ResourceLocation;
@@ -47,29 +47,25 @@ public class ExampleTrees {
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		
-		populateBiomeDataBase();
+		WorldGenRegistry.registerBiomeDataBasePopulator(populator);
 		
 		proxy.init();
 	}
 
-	//Populate the Biome DataBase with generation instructions
-	public void populateBiomeDataBase() {
-		
-		if(WorldGenRegistry.isWorldGenEnabled()) {
+	public IBiomeDataBasePopulator populator = new IBiomeDataBasePopulator() {
+		@Override
+		public void populate(BiomeDataBase dbase) {
 			Species ironTree = TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, "iron"));
 			ISpeciesSelector ironTreeSelector = new StaticSpeciesSelector(ironTree);
-			BiomeDataBase dbase = TreeGenerator.getTreeGenerator().biomeDataBase;
 			
 			Biome.REGISTRY.forEach(biome -> {
-					if(BiomeDictionary.hasType(biome, Type.MESA)) {	//We want this tree to generate in mesa biomes
-						dbase.setSpeciesSelector(biome, ironTreeSelector, Operation.REPLACE);
-						dbase.setChanceSelector(biome, (rnd, spc, rad) -> { return rnd.nextFloat() < 0.025f ? EnumChance.OK : EnumChance.UNHANDLED; }, Operation.SPLICE_BEFORE);
-						dbase.setDensitySelector(biome, (rnd, nd) -> { return -1; } , Operation.SPLICE_BEFORE);
-					}
+				if(BiomeDictionary.hasType(biome, Type.MESA)) {	//We want this tree to generate in mesa biomes
+					dbase.setSpeciesSelector(biome, ironTreeSelector, Operation.REPLACE);
+					dbase.setChanceSelector(biome, (rnd, spc, rad) -> { return rnd.nextFloat() < 0.025f ? EnumChance.OK : EnumChance.UNHANDLED; }, Operation.SPLICE_BEFORE);
+					dbase.setDensitySelector(biome, (rnd, nd) -> { return -1; } , Operation.SPLICE_BEFORE);
 				}
-			);
-		}
-		
-	}
+			});
+		}	
+	};
 	
 }
