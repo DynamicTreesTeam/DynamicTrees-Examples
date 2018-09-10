7e2de76d23d1d8d704ae62ab438c348ac5b55daf
ferreusveritas <ferreusveritas@gmail.com>
2018-09-10 14:55:29 -0400

    Fix unlocalized names to include modid to prevent naming collisions

--- src/main/java/com/ferreusveritas/exampletrees/blocks/BlockIronLog.java
+++ src/main/java/com/ferreusveritas/exampletrees/blocks/BlockIronLog.java
@@ -8,7 +8,7 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 
 public class BlockIronLog extends BlockLog {
-
+	
 	public static final String name = "ironlog";
 	
 	public BlockIronLog() {
@@ -17,46 +17,47 @@ public class BlockIronLog extends BlockLog {
 	}
 	
 	public BlockIronLog(String name) {
-        this.setDefaultState(this.blockState.getBaseState().withProperty(LOG_AXIS, BlockLog.EnumAxis.Y));
+		this.setDefaultState(this.blockState.getBaseState().withProperty(LOG_AXIS, BlockLog.EnumAxis.Y));
 		setDefaultState(this.blockState.getBaseState());
-		setUnlocalizedName(name);
 		setRegistryName(name);
+		setUnlocalizedName(getRegistryName().toString());
 	}
 	
-    @Override
+	@Override
 	protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] {LOG_AXIS});
-    }
+		return new BlockStateContainer(this, new IProperty[] {LOG_AXIS});
+	}
 	
-    @Override
+	@Override
 	public IBlockState getStateFromMeta(int meta) {
-    	    	
-        IBlockState iblockstate = this.getDefaultState();
-
-        switch (meta & 12) {
-            case 0: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);  break;
-            case 4: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);  break;
-            case 8: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);  break;
-            default: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
-        }
-        
-        return iblockstate;
-    }
-
-    /**
-     * Convert the BlockState into the correct metadata value
-     */
-    @Override
+		
+		IBlockState iblockstate = this.getDefaultState();
+		
+		switch (meta & 12) {
+			case 0: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Y);  break;
+			case 4: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.X);  break;
+			case 8: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.Z);  break;
+			default: iblockstate = iblockstate.withProperty(LOG_AXIS, BlockLog.EnumAxis.NONE);
+		}
+		
+		return iblockstate;
+	}
+	
+	/**
+	 * Convert the BlockState into the correct metadata value
+	 */
+	@Override
 	public int getMetaFromState(IBlockState state) {
-        int i = 0;
-
-        switch (state.getValue(LOG_AXIS)) {
-            case X:  i |= 4; break;
-            case Z:  i |= 8; break;
-            case NONE: i |= 12; break;
-            default: break;
-        }
-        
-        return i;
-    }
+		int i = 0;
+		
+		switch (state.getValue(LOG_AXIS)) {
+			case X:  i |= 4; break;
+			case Z:  i |= 8; break;
+			case NONE: i |= 12; break;
+			default: break;
+		}
+		
+		return i;
+	}
+	
 }
