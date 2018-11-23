7dcdcd90cb59dcf9b9ac9c806f65ee772aa4089d
ferreusveritas <ferreusveritas@gmail.com>
2018-11-23 15:32:06 -0500

    Adjust version to kinda match Dynamic Trees

--- src/main/java/com/ferreusveritas/exampletrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/exampletrees/ModConstants.java
@@ -3,6 +3,6 @@ package com.ferreusveritas.exampletrees;
 public class ModConstants {
 
 	public static final String MODID = "exampletrees";
-	public static final String VERSION = "0.6.7";
+	public static final String VERSION = "0.9.1";
 	
 }
