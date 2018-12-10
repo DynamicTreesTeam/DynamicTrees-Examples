12cf93eb76510a4fdbf4beba6aad16642126d7dd
ferreusveritas <ferreusveritas@gmail.com>
2018-12-10 11:34:01 -0500

    Event driven BiomeDataBasePopulator registration

--- src/main/java/com/ferreusveritas/exampletrees/ModTrees.java
+++ src/main/java/com/ferreusveritas/exampletrees/ModTrees.java
@@ -5,11 +5,18 @@ import java.util.Collections;
 
 import com.ferreusveritas.dynamictrees.api.TreeBuilder;
 import com.ferreusveritas.dynamictrees.api.TreeRegistry;
+import com.ferreusveritas.dynamictrees.api.WorldGenRegistry.BiomeDataBasePopulatorRegistryEvent;
+import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.EnumChance;
+import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.ISpeciesSelector;
+import com.ferreusveritas.dynamictrees.api.worldgen.BiomePropertySelectors.StaticSpeciesSelector;
+import com.ferreusveritas.dynamictrees.api.worldgen.IBiomeDataBasePopulator;
 import com.ferreusveritas.dynamictrees.systems.dropcreators.DropCreatorHarvest;
 import com.ferreusveritas.dynamictrees.systems.featuregen.FeatureGenFruit;
 import com.ferreusveritas.dynamictrees.trees.Species;
 import com.ferreusveritas.dynamictrees.trees.SpeciesRare;
 import com.ferreusveritas.dynamictrees.trees.TreeFamily;
+import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase;
+import com.ferreusveritas.dynamictrees.worldgen.BiomeDataBase.Operation;
 import com.ferreusveritas.exampletrees.trees.TreeIron;
 
 import net.minecraft.block.BlockNewLeaf;
@@ -19,7 +26,13 @@ import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber(modid = ModConstants.MODID)
 public class ModTrees {
 
 	//Sometimes it helps to cache a few blockstates
@@ -59,6 +72,28 @@ public class ModTrees {
 		
 		//Register the individual species not part of a new tree
 		Species.REGISTRY.register(bunnyOak);
+	}
+
+	@SubscribeEvent
+	public static void registerDataBasePopulators(final BiomeDataBasePopulatorRegistryEvent event) {
+		
+		IBiomeDataBasePopulator populator = new IBiomeDataBasePopulator() {
+			@Override
+			public void populate(BiomeDataBase dbase) {
+				Species ironTree = TreeRegistry.findSpecies(new ResourceLocation(ModConstants.MODID, "iron"));
+				ISpeciesSelector ironTreeSelector = new StaticSpeciesSelector(ironTree);
+				
+				Biome.REGISTRY.forEach(biome -> {
+					if(BiomeDictionary.hasType(biome, Type.MESA)) {	//We want this tree to generate in mesa biomes
+						dbase.setSpeciesSelector(biome, ironTreeSelector, Operation.REPLACE);
+						dbase.setChanceSelector(biome, (rnd, spc, rad) -> { return rnd.nextFloat() < 0.025f ? EnumChance.OK : EnumChance.UNHANDLED; }, Operation.SPLICE_BEFORE);
+						dbase.setDensitySelector(biome, (rnd, nd) -> { return -1; } , Operation.SPLICE_BEFORE);
+					}
+				});
+			}	
+		};
+				
+		event.register(populator);
 	}
 	
 }
