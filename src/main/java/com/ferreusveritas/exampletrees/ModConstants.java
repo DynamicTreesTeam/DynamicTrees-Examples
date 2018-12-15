5df2ea98c3d8a26262fd8e1b05f02840d77d2063
ferreusveritas <ferreusveritas@gmail.com>
2018-12-15 12:09:18 -0500

    Adopt standard add-on versioning

--- src/main/java/com/ferreusveritas/exampletrees/ModConstants.java
+++ src/main/java/com/ferreusveritas/exampletrees/ModConstants.java
@@ -1,8 +1,8 @@
 package com.ferreusveritas.exampletrees;
 
 public class ModConstants {
-
+	
 	public static final String MODID = "exampletrees";
-	public static final String VERSION = "0.9.1";
+	public static final String VERSION = "1.12.2-9999.9999.9999z";//Maxed out version to satisfy dependencies during dev, Assigned from gradle during build, do not change
 	
 }
